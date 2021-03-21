bde5a6cc1ebd9066890d353e0701de1e53fbad8f
Rafael Zimmer <rzimmerdev@gmail.com>
2021-03-21 16:05:11 -0300

    Create constant.py

---
+++ constant.py
@@ -0,0 +1,5 @@
+API_TOKEN = "1700885261:AAETCokNpqNDk44x3d5XASfnQfzxiNOKWfI"
+
+CONTATINHOS_SHEET_LINK = "https://docs.google.com/spreadsheets/d/1Kfy-tCDA_UggPUOaYs1w9oN_DtuL6GBWPyCmcl_R3f8/edit?usp=sharing"
+GITHUB_REPO_LINK = "https://github.com/lineuzinho-icmc/lineuzinho"
+SAVED_DOCS_LINK = "https://t.me/docs21"
