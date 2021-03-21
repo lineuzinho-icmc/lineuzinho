e658af7f93d54143af9119ccdd3cef1b08b42922
Rafael Zimmer <rzimmerdev@gmail.com>
2021-03-21 17:05:06 -0300

    added environ variables

--- constant.py
+++ constant.py
@@ -1,16 +1,7 @@
 import os
 import sys
 
-if __name__ == "__main__":
-    try:
-        with open("safe.txt",'w',encoding = 'utf-8') as f:
-            f.write(sys.argv[1])
-        print("Saved key: " + sys.argv[1])
-    except:
-    	print("Please, add API key to console call")
-        
-safe = open("safe.txt", "r")
-api = safe.read()
+api = os.environ["SECRET"]
 
 sheet = "https://docs.google.com/spreadsheets/d/1Kfy-tCDA_UggPUOaYs1w9oN_DtuL6GBWPyCmcl_R3f8/edit?usp=sharing"
 github = "https://github.com/lineuzinho-icmc/lineuzinho"
