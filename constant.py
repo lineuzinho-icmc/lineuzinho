eda2cf2ebed8c2418977188048b51b5793f261d0
Benício Januário <benicio.januario@ifood.com.br>
2021-03-21 15:53:02 -0300

    Remove sensitive data

--- constant.py
+++ constant.py
@@ -1,5 +1,5 @@
-API_TOKEN = "1700885261:AAETCokNpqNDk44x3d5XASfnQfzxiNOKWfI"
+API_TOKEN = "api token"
 
-CONTATINHOS_SHEET_LINK = "https://docs.google.com/spreadsheets/d/1Kfy-tCDA_UggPUOaYs1w9oN_DtuL6GBWPyCmcl_R3f8/edit?usp=sharing"
-GITHUB_REPO_LINK = "https://github.com/lineuzinho-icmc/lineuzinho"
-SAVED_DOCS_LINK = "https://t.me/docs21"
+CONTATINHOS_SHEET_LINK = "contatinhos"
+GITHUB_REPO_LINK = "repo"
+SAVED_DOCS_LINK = "docs channel"
