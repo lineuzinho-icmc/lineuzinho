2c3f481a5a1328edf6e912a16f5f5807a3ea7634
Rafael Zimmer <rzimmerdev@gmail.com>
2021-03-21 12:20:29 -0300

    replaced commit on previous origin

--- main.py
+++ main.py
@@ -1,16 +1,49 @@
-from telegram.ext import Updater, CommandHandler
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 import logging
+import random
 
 API_TOKEN = "1700885261:AAETCokNpqNDk44x3d5XASfnQfzxiNOKWfI"
+
 CONTATINHOS_SHEET_LINK = "https://docs.google.com/spreadsheets/d/1Kfy-tCDA_UggPUOaYs1w9oN_DtuL6GBWPyCmcl_R3f8/edit?usp=sharing"
+GITHUB_REPO_LINK = "https://github.com/lineuzinho-icmc/lineuzinho"
+SAVED_DOCS_LINK = "https://t.me/docs21"
+
+DOCS_CHANNEL_MESSAGES_MAX = 800
 
 def start(update, context):
-    s = "pó fala meu rei"
-    update.message.reply_text(s)
+    update.message.reply_text("pó fala meu rei")
 
 def contatinhos(update, context):
-    update.message.reply_text("CHAMA NO CONTATINHO")
-    update.message.reply_text(CONTATINHOS_SHEET_LINK)
+    update.message.reply_text("CHAMA NOS CONTATINHO")
+    update.message.reply_text(CONTATINHOS_SHEET_LINK, disable_web_page_preview=True)
+
+def repo(update, context):
+    update.message.reply_text(GITHUB_REPO_LINK)
+
+def help(update, context):
+    update.message.reply_text("digita \"/\" no teclado pra dar uma olhada nos comandos disponíveis :V")
+
+def save(update, context):
+    originalMessage = update.message.reply_to_message
+    if not originalMessage or not originalMessage.text:
+        update.message.reply_text("faz o comando respondendo alguma coisa...", parse_mode="Markdown")
+
+    context.bot.forwardMessage("@docs21", update.effective_chat.id, originalMessage.message_id)
+
+def docsChannel(update, context):
+    update.message.reply_text(SAVED_DOCS_LINK)
+
+def newMembersGreetings(update, context):
+    newMembers = update.message.new_chat_members
+    welcomeVocative = ""
+    if len(newMembers) > 1:
+        for newMember in newMembers[:-1]:
+            welcomeVocative += "{0}, ".format(newMember.first_name.split(" ")[0].capitalize())
+        welcomeVocative += " e {0}".format(newMembers[-1].first_name.split(" ")[0].capitalize())
+    else:
+        welcomeVocative = newMembers[0].first_name.split(" ")[0].capitalize()
+    
+    update.message.reply_text("Boas vindas, {0} rsrs".format(welcomeVocative))
 
 def main():
     logger = logging.getLogger(__name__)
@@ -22,12 +55,18 @@ def main():
     dp = updater.dispatcher
 
     dp.add_handler(CommandHandler("start", start))
+    dp.add_handler(CommandHandler("save", save))
+    dp.add_handler(CommandHandler("docs", docsChannel))
     dp.add_handler(CommandHandler("contatinhos", contatinhos))
+    dp.add_handler(CommandHandler("repo", repo))
+    dp.add_handler(CommandHandler("help", help))
+
+    dp.add_handler(MessageHandler(Filters.status_update.new_chat_members, newMembersGreetings))
 
     updater.start_polling()
-    logging.info("=== Bot running! ===")
+    logging.info("=== Lineuzinho up&running! ===")
     updater.idle()
-    logging.info("=== Bot shutting down! ===")
+    logging.info("=== Lineuzinho shutting down :( ===")
 
 if __name__ == "__main__":
     main()
