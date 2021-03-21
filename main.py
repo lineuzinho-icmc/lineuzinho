0c1a6d0a286da7982c9b15ff6a3571890375865d
Rafael Zimmer <rzimmerdev@gmail.com>
2021-03-21 14:55:25 -0300

    Update main.py

--- main.py
+++ main.py
@@ -21,6 +21,20 @@ def contatinhos(update, context):
     update.message.reply_text("CHAMA NOS CONTATINHO")
     update.message.reply_text(CONTATINHOS_SHEET_LINK, disable_web_page_preview=True)
 
+def links(update, context):
+    update.message.reply_text("""
+    LINKS ÚTEIS 
+
+    Link do grupo BCC 021: estamos adicionando todo mundo aos poucos. Se puder ajudar a achar o pessoal, tá ai: https://t.me/joinchat/ufkXtyUrI5MzZTVh
+
+    Inscrição na semana de recepção: calouros.icmc.usp.br/
+
+    Guia do Bixo: https://www.notion.so/Guia-do-Bixo-das-Gal-xias-EAD-Edition-1f0243e5db4b4b9a90cf6fa029a328dc#44e5291de7894f0184bc8bdfc574a4fe
+    
+    Contatinho de geral: https://docs.google.com/spreadsheets/d/1Kfy-tCDA_UggPUOaYs1w9oN_DtuL6GBWPyCmcl_R3f8/edit?usp=sharing
+
+    BO FAZER BOT https://www.gabekanegae.com/creating-a-telegram-bot-with-python/""")    
+    
 def repo(update, context):
     update.message.reply_text(GITHUB_REPO_LINK)
 
