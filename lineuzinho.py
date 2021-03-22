fc45b8f83cefbc11b11e6760663d9f9e6113fe74
Benício Januário <benicio.januario@ifood.com.br>
2021-03-22 09:52:13 -0300

    Update shortened contatinhos link

--- lineuzinho.py
+++ lineuzinho.py
@@ -7,7 +7,7 @@ from beaner import Beaner
 class Lineuzinho:
     def __init__(self):
         self.API_TOKEN = "1700885261:AAETCokNpqNDk44x3d5XASfnQfzxiNOKWfI" ## os.environ["SECRET"]
-        self.contatinhosSheet = "https://shorturl.at/nMOV2"
+        self.contatinhosSheet = "http://bit.ly/contatosbcc021"
         self.githubRepo = "https://github.com/lineuzinho-icmc/lineuzinho"
         self.usefulLinks = "Estamos adicionando todo mundo aos poucos. Se puder ajudar a achar o pessoal, passa o link do grupo na descrição!\n\nInscrição na semana de recepção: calouros.icmc.usp.br/\n\nGuia do Bixo: https://bit.ly/3c9mcUG\n\nContatinho de geral: {0}\n\n".format(self.contatinhosSheet)
         self.docsChannel = "https://t.me/docs21"
