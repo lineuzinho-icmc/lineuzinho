6350691411e1ccd7ec69ae1d8ff8e50c3b1cabab
Rafael Zimmer <rzimmerdev@gmail.com>
2021-03-23 16:17:58 -0300

    finished pi_index finished random activity announcement

--- db.py
+++ db.py
@@ -2,5 +2,20 @@ import os, psycopg2
 
 class Connection():
     DATABASE_URL = os.environ['DATABASE_URL']
-    def __init__(self):
+
+    def execute_sql(self, sql):
         conn = psycopg2.connect(self.DATABASE_URL, sslmode='require')
+        cursor = conn.cursor()
+        cursor.execute(sql)
+        conn.commit()
+        cursor.close()
+        conn.close()
+
+    def get_members(self):
+        conn = psycopg2.connect(self.DATABASE_URL, sslmode='require')
+        cursor = conn.cursor()
+        cursor.execute("SELECT * FROM members")
+        result = cursor.fetchall()
+        cursor.close()
+        conn.close()
+        return result
