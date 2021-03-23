339b6febc3765807053973d26b525dccb921b608
Rafael Zimmer <rzimmerdev@gmail.com>
2021-03-23 13:30:33 -0300

    fixed db

--- db.py
+++ db.py
@@ -1,6 +1,6 @@
 import os, psycopg2
 
 class Connection():
-    self.DATABASE_URL = os.environ['DATABASE_URL']
+    DATABASE_URL = os.environ['DATABASE_URL']
     def __init__(self):
         conn = psycopg2.connect(self.DATABASE_URL, sslmode='require')
