2832c1a32f99b99a196bb33eedcad9f383caf982
Rafael Zimmer <rzimmerdev@gmail.com>
2021-04-16 15:54:27 -0300

    added new group

--- lineuzinho.py
+++ lineuzinho.py
@@ -15,7 +15,7 @@ class Lineuzinho:
         self.contatinhosSheet = "http://bit.ly/contatosbcc021"
         self.githubRepo = "https://github.com/lineuzinho-icmc/lineuzinho"
         self.lives = "Live da primeira semana: http://bit.ly/aberturadasemana \n\nLive do JB: bit.ly/calldojb\n\n"
-        self.subjectsGroupsLinks = "\nICC1: https://t.me/joinchat/cxOk_L889nc2NDk5\n\nLAB ICC1 [Tarde]: https://t.me/joinchat/4KTLgPVis483NDUx\n\nLAB ICC1 [Noite]: https://t.me/joinchat/23qmmmmAnCg1ZDQx\n\nEletrônica: https://t.me/joinchat/0oBPQIAN17kwZTNh\n\nCálculo 1: https://t.me/joinchat/loEfiwcvAV82ZmVh\n\nG.A: https://t.me/joinchat/3i-Kuf256mo1ODA5\n\nILD: https://t.me/joinchat/TItjZehheg44NjIx\n\nPLD https://t.me/joinchat/ClHA1xPsSBRjZWUx"
+        self.subjectsGroupsLinks = "\nICC1: https://t.me/joinchat/cxOk_L889nc2NDk5\n\nLAB ICC1 [Tarde]: https://t.me/joinchat/4KTLgPVis483NDUx\n\nLAB ICC1 [Noite]: https://t.me/joinchat/23qmmmmAnCg1ZDQx\n\nEletrônica: https://t.me/joinchat/0oBPQIAN17kwZTNh\n\nCálculo 1: https://t.me/joinchat/loEfiwcvAV82ZmVh\n\nG.A: https://t.me/joinchat/3i-Kuf256mo1ODA5\n\nILD: https://t.me/joinchat/TItjZehheg44NjIx\n\nPLD https://t.me/joinchat/ClHA1xPsSBRjZWUx\n\nAVISOS: https://t.me/joinchat/B5qbuFd8x483ZThh"
         self.usefulLinks = "Estamos adicionando todo mundo aos poucos. Se puder ajudar a achar o pessoal, passa o link do grupo na descrição!\n\nInscrição na semana de recepção: calouros.icmc.usp.br/\n\nGuia do Bixo: https://bit.ly/3c9mcUG\n\nContatinho de geral: {0}\n\nEnquetes: https://t.me/joinchat/qrJ_MrnHDbE1ZmNh\n\nDrive com livros: http://bit.ly/livrosbccicmc\n\n".format(self.contatinhosSheet)
         self.docsChannel = "https://t.me/docs21"
 
