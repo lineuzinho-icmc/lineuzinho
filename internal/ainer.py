8e8f89f4fcc9e3e74ead784f7d69d67133d56731
Benicio Januario <pbenicio.januario@gmail.com>
2021-04-18 17:31:52 -0300

    Rectify listing directory path

--- internal/ainer.py
+++ internal/ainer.py
@@ -4,7 +4,7 @@ from random import choice
 class Ainer:
     def __init__(self):
         self.ainRepo = './resources/ains' # Relative to project's root
-        self.ains = listdir(self.ainRepo)
+        self.ains = listdir('../resources/ains') # Relative to class file
 
     def getRandomAinPath(self):
         return path.join(self.ainRepo, choice(self.ains))
