86b43049cced5434058441a9a5ccee763c6eeb75
Benicio Januario <pbenicio.januario@gmail.com>
2021-04-18 17:49:48 -0300

    Remove relative path declaration

--- internal/ainer.py
+++ internal/ainer.py
@@ -3,8 +3,11 @@ from random import choice
 
 class Ainer:
     def __init__(self):
-        self.ainRepo = './resources/ains' # Relative to project's root
-        self.ains = listdir('../resources/ains') # Relative to class file
+        self.ainRepo = 'resources/ains' # Relative to project's root
+        self.ains = listdir(self.ainRepo) # Relative to class file
 
     def getRandomAinPath(self):
         return path.join(self.ainRepo, choice(self.ains))
+
+oi = Ainer()
+print(oi.getRandomAinPath())
