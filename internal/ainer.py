3ee61a27184dc1cb9ebf97be207fe4729d28fb0b
Rafael Zimmer <rzimmerdev@gmail.com>
2021-04-18 00:28:01 -0300

    fixed directory reference

--- internal/ainer.py
+++ internal/ainer.py
@@ -4,7 +4,7 @@ from random import choice
 class Ainer:
     def __init__(self):
         self.ainRepo = './resources/ains' # Relative to project's root
-        self.ains = listdir(path.join('..', self.ainRepo))
+        self.ains = listdir(self.ainRepo)
 
     def getRandomAinPath(self):
         return path.join(self.ainRepo, choice(self.ains))
